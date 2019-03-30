c2b06ae9a7d46c3fc0521dcc40a59104934bc4b2
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-30 07:57:06 -0400

    new IP

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt')
-var MQTT_TOPIC = "homeGet/light";
+var MQTT_TOPIC = "WX";
 var MQTT_ADDR = "127.0.0.1:1883";
 var MQTT_PORT = 1883;
 var client  = mqtt.connect(MQTT_ADDR,{clientId: "webClient", keeplive: 1, clean: false, debug:true});
@@ -13,9 +13,6 @@ var app = express();
 //Create a server on localhost:5000
 //var server = app.listen(process.env.PORT || 5000);
 
-var server = app.listen((process.env.PORT || 3000,) function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
 //host content as static on public
 app.use(express.static('public'));
 
@@ -24,18 +21,21 @@ console.log("Node is running on port 5000...");
 //MQTT
 client.on('connect', function () {
     client.subscribe(MQTT_TOPIC, { qos: 2 });
-    client.publish(MQTT_TOPIC, '1000');
 });
+
 client.on('message', function (topic, message) {
     // message is Buffer
     console.log(message.toString());
 });
+
 client.on('error', function(){
     console.log("ERROR")
 })
+
 client.on('offline', function() {
     console.log("offline");
 });
+
 client.on('reconnect', function() {
     console.log("reconnect");
 });
