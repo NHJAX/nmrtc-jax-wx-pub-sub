f95e93dded895320b2a306f374e3175dfab269a4
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-01 06:52:10 -0400

    new comments

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ var settings = {
 		port:1883
   };
 var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://10.0.0.205');
-var server = new mosca.Server(settings);
+var client  = mqtt.connect('mqtt://10.0.0.205');//IP off the machine which the server is hosted
 
-server.on('ready', function(){
-console.log("ready");
+var server = new mosca.Server('Server setting up', settings);
+
+server.on('Server ready', function(){
 });
-
 
 client.on('connect', function () {
     client.subscribe('WX')
+    console.log("Client has been detected");
 });
+
 client.on('message', function (topic, message) {
 context = message.toString();
 console.log(context)
