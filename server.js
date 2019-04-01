70d6b8e1826fc62ed7b4d65b9a61bd5ceb6058ea
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-01 10:39:31 -0400

    test

--- server.js
+++ server.js
@@ -16,19 +16,17 @@ server.on('ready', function(){
 //MQTT Message handler
 client.on('connect', function () {
     client.subscribe('jax')
-    console.log("JAX client has been detected");
     client.subscribe('mayport')
-    console.log("Mayport client has been detected");
     client.subscribe('keywest')
-    console.log("Keywest client has been detected");
     client.subscribe('albany')
-    console.log("Albany client has been detected");
     client.subscribe('kingsbay')
-    console.log("Kingsbay client has been detected");
+    console.log("All clients subscribed and O N L I N E");
 });
 
 client.on('message', function (topic, message) {
 m = message.toString();
 t = topic.toString();
 console.log(t, m);
+s = (t + m);
+console.log(s);
 });
